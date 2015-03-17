1aa0ac12d4b37e3d8a47b024098b4f364aac99ae
Dave Rostron <yastero@gmail.com>
2015-03-17 12:01:30 -0700

    update PigLatiniseTest

--- src/test/scala/PigLatiniseTest.scala
+++ src/test/scala/PigLatiniseTest.scala
@@ -3,12 +3,13 @@ import utest._
 
 object PigLatinSuite extends TestSuite {
 
+  // examples from http://en.wikipedia.org/wiki/Pig_Latin
+
   val tests = TestSuite {
     "pigLatinise" - {
       "words that begin with consonant sounds" - {
         assert(pigLatinise("pig") == "igpay")
         assert(pigLatinise("banana") == "ananabay")
-        assert(pigLatinise("trash") == "ashtray")
         assert(pigLatinise("happy") == "appyhay")
         assert(pigLatinise("duck") == "uckday")
         assert(pigLatinise("glove") == "oveglay")
